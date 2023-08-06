3d436ef4cb9f5d0eb8dc19680af1f1edf4741a81
Alexander Poole <alex.o.poole@gmail.com>
2023-08-06 21:26:08 +0200

    Create keymap with home row modifiers

--- keyboards/handwired/scylla/config.h
+++ keyboards/handwired/scylla/config.h
@@ -24,3 +24,9 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
