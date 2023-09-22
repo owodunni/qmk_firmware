7771fb4dc1a2b927abe095e8dea8abd27b5bf928
Alexander Poole <alex.o.poole@gmail.com>
2023-09-22 11:01:07 +0200

    Enable mouse keys and movements in qmk

--- keyboards/handwired/scylla/config.h
+++ keyboards/handwired/scylla/config.h
@@ -30,3 +30,6 @@
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
+#define MOUSEKEY_WHEEL_INTERVAL 60
+#define MOUSEKEY_WHEEL_DELTA 2
+#define MOUSEKEY_INTERVAL 10
