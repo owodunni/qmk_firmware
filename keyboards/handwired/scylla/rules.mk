cf2e369519d94fefb5e05fea0af2f416511d149a
Alexander Poole <alex.o.poole@gmail.com>
2023-09-22 11:01:07 +0200

    Enable mouse keys and movements in qmk

--- keyboards/handwired/scylla/rules.mk
+++ keyboards/handwired/scylla/rules.mk
@@ -5,7 +5,7 @@ ALLOW_WARNINGS = yes
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
